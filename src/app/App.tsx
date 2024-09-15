bc244e7de26dc1b22cce3ad6178d0d80a8f6731d
Konstantin Zaytcev <zaydisk@yandex.ru>
2024-09-15 11:47:47 +0300

    changed App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          <b>Цель</b>: Изучить архитектуру современных frontend приложений на примере React.
+        </p>
+        <p>Web-разработчик (PHP, GO, JS, Базы данных, RabbitMQ, Docker, CI/CD)</p>
+        <p>Проектирую, разрабатываю и внедряю CRM и ERP системы.</p>
+        <p>
+          Мое портфолио:{' '}
+          <a href={'https://zaytcev.com'} target={'_blank'} rel="noreferrer">
+            zaytcev.com
+          </a>
         </p>
       </header>
     </div>
