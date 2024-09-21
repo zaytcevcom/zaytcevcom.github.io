0bde975d36050f61bf039b7f732b1ac0fbbae5bd
Konstantin Zaytcev <zaydisk@yandex.ru>
2024-09-21 13:08:58 +0300

    completed hw 01

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,127 @@
  * - type ('Profit')
  * */
 
+export type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+enum OperationType {
+  Cost = 'Cost',
+  Profit = 'Profit',
+}
+
+interface OperationBase {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+}
+
+interface Cost extends OperationBase {
+  type: OperationType.Cost;
+}
+
+interface Profit extends OperationBase {
+  type: OperationType.Profit;
+}
+
+export type Operation = Cost | Profit;
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  const randomCategoryId = Math.floor(Math.random() * categories.length);
+  const randomCategory = categories[randomCategoryId];
+
+  const randomProductId = Math.floor(Math.random() * randomCategory.products.length);
+  const randomProduct = randomCategory.products[randomProductId];
+
+  const category: Category = {
+    id: randomCategoryId.toString(),
+    name: randomCategory.name,
+    photo: null,
+  };
+
+  return {
+    id: randomProductId.toString(),
+    name: randomProduct,
+    photo: null,
+    desc: 'Описание для ' + randomProduct,
+    createdAt: createdAt,
+    oldPrice: getRandomNumber(100, 20000),
+    price: getRandomNumber(100, 20000),
+    category: category,
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const randomCategoryId = Math.floor(Math.random() * categories.length);
+  const randomCategory = categories[randomCategoryId];
+
+  const category: Category = {
+    id: randomCategoryId.toString(),
+    name: randomCategory.name,
+    photo: null,
+  };
+
+  return {
+    id: randomCategoryId.toString(),
+    name: randomCategory.name,
+    desc: 'Описание для ' + randomCategory.name,
+    createdAt: createdAt,
+    amount: getRandomNumber(1200, 7000),
+    category: category,
+    type: getRandomNumber(0, 1) === 1 ? OperationType.Cost : OperationType.Profit,
+  };
+};
+
+const categories: { name: string; products: string[] }[] = [
+  {
+    name: 'Еда и напитки',
+    products: [
+      'яблоко',
+      'банан',
+      'клубника',
+      'помидор',
+      'картофель',
+      'лук',
+      'морковь',
+      'апельсин',
+      'виноград',
+      'груша',
+    ],
+  },
+  {
+    name: 'Домашний декор',
+    products: ['диван', 'кресло', 'кровать', 'стол', 'стул'],
+  },
+  {
+    name: 'Спорт и отдых',
+    products: ['велосипед', 'ролики', 'лыжи', 'мяч', 'гантели'],
+  },
+];
+
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
